5d304598f4c47588614ea700c196f9f22d188f18
John Ajiboye <ajiboye.o.john@gmail.com>
2018-03-31 14:03:26 +0300

    fixed issue with unequal card height

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var eShop = angular.module('eShop',['ui.bootstrap']);
 
 eShop.controller('mainCtrl',function($scope,$http){
-
+  
 $scope.products='';
 $scope.title = 'Erply-Eshop';
 $scope.cartMessage = 'Add to Cart';
@@ -15,24 +15,38 @@ $scope.getProduct = function() {
       }).then(function succesCallBack(response){
         $scope.res = response.data;
         console.log($scope.res)
+       
+        $scope.filtered = $scope.res.filter((a) =>{
+            return a.id == 1;
+        });
+        console.log($scope.filtered)
         angular.forEach($scope.res, function (result) {
-            
+          
         console.log("NAME: "+result.name)
         $scope.name= result.name;
-        console.log("$nameiiiiiii: "+$scope.name)
+       
            $scope.price= result.price;
            $scope.image =result.image ;
            $scope.detail=result.description ;
            $scope.stock=result.Instock;
-        });
- 
-          
-         
-            
-          
-          
-
-      })
+        }); 
+    })
 }
-    
+$scope.getProductDetails = function() {
+    console.log("getting details");
+$http({
+    method: 'GET',
+    url: 'https://erply-challenge.herokuapp.com/list?AUTH=fae7b9f6-6363-45a1-a9c9-3def2dae206d'
+  }).then(function succesCallBack(response){
+    $scope.res = response.data;
+    console.log( $scope.res);
+    var i = 0;
+    for(var i=1; i< $scope.res.length;i++) {
+        if ($scope.res.id[i] == id){
+            return $scope.res.description;
+        }
+    }
+ 
+})
+}
 })
