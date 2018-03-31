702bc6c076bf5fa7cc75a32f365803a3aaaa4ec9
John Ajiboye <ajiboye.o.john@gmail.com>
2018-04-01 01:42:01 +0300

    updated architecture and routes

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-// Serve only the static files form the current directory
-app.use(express.static(__dirname));
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
